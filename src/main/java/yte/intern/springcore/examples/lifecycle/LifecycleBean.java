feae3296110583ebf708dafad7207554dee0e62c
fatih.dogmus <fatih.dogmus@tubitak.gov.tr>
2023-08-09 16:33:02 +0300

    javax -> jakarta migration

--- src/main/java/yte/intern/springcore/examples/lifecycle/LifecycleBean.java
+++ src/main/java/yte/intern/springcore/examples/lifecycle/LifecycleBean.java
@@ -2,8 +2,8 @@ package yte.intern.springcore.examples.lifecycle;
 
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
 public class LifecycleBean {
